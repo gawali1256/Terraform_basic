7044677b58d77bfb1d288ef9701f19c566a6fd21
sanket gawali <102585672+gawali1256@users.noreply.github.com>
2023-09-09 12:12:30 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
-    # We recommend pinning to the specific version of the Docker Provider you're using
-    # since new versions are released frequently
+
     docker = {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
 
+     aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     
   }
 }
