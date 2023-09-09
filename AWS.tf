040ac686647db6f85dcd746155370a64e51617bd
sanket gawali <102585672+gawali1256@users.noreply.github.com>
2023-09-09 12:47:47 +0530

    Update AWS.tf

--- AWS.tf
+++ AWS.tf
@@ -14,3 +14,35 @@ resource "aws_instance" "demoec2"{
       }
       
 }
+
+resources "aws_key_pair" "mykey"{
+      key_name = "terra-key"
+      public_key = file ("path")
+}
+
+resource "aws_default_vpc" "default_vpc"{
+}
+
+resource "aws_instance" "my-vpc-instance"{
+      key_name = aws_key_pair.mykey.key_name
+}
+
+
+resource "aws_security_group" "allow_ssh"{
+      name = "allow_ssh"
+      description = "Allow ssh inbound traffic"
+      vpc_id = aws_default_vpc.default_vpc.id
+
+ingress{
+      description = "TLS from VPC"
+      from_port = 22 
+      to_port = 22
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+}
+tags={
+      Name = "allow_ssh"
+      }
+}
+
+
