bdd54adf34d1e7630b29081af3b4e37f3df4cfd3
sanket gawali <102585672+gawali1256@users.noreply.github.com>
2023-09-09 12:25:21 +0530

    Update AWS.tf

--- AWS.tf
+++ AWS.tf
@@ -2,6 +2,15 @@ provider "aws" {
       region = "ap-south1"
 }
 
-resource "aws+s3_bucket" "demo-s3"{
-    bucket = "terraform-new"
+resource "aws_s3_bucket" "demo-s3"{
+      bucket = "terraform-new"
+}
+
+resource "aws_instance" "demoec2"{
+      ami = "ami-02bb7d8191b50f4bb"
+      instance_type = "t2.micro"
+      tags={
+            Name = "terra-instance"
+      }
+      
 }
